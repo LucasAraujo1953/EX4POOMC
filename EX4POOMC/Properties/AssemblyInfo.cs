e34181930c485eb7524485f693c9cd3f4a376627
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-08-31 18:30:10 -0300

    Add files via upload

---
+++ EX4POOMC/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("EX4POOMC")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ES00007907")]
+[assembly: AssemblyProduct("EX4POOMC")]
+[assembly: AssemblyCopyright("Copyright © ES00007907 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("a178a85c-857c-4bf4-a35e-349b56912055")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ EX4POOMC/Triangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EX4POOMC
+{
+    internal class Triangulo
+    {
+        private double baseTriangulo;
+
+        private double alturaTriangulo;
+
+
+
+        public Triangulo(double baseTriangulo, double alturaTriangulo)
+
+        {
+
+            this.baseTriangulo = baseTriangulo;
+
+            this.alturaTriangulo = alturaTriangulo;
+
+        }
+
+
+
+        public double CalcularArea()
+
+        {
+
+            return(baseTriangulo * alturaTriangulo) / 2;
+
+        }
+    }
+}
